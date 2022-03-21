f5217927e8f2adbd88623250409c9e64bc5fd2d8
znjs <jasrikar@gmail.com>
2022-03-21 22:46:05 +0530

    intial commit && added file2,3,4,5
    file7

--- file7.js
+++ file7.js
@@ -1,1 +1,28 @@
-console.log("file 7, line 1");
+let items = new WeakMap();
+
+class Stack {
+  constructor() {
+    items.set(this, []);
+  }
+  push(element) {
+    let s = items.get(this);
+    s.push(element);
+  }
+  pop() {
+    let s = items.get(this);
+    let r = s.pop();
+    console.log(r);
+    return r;
+  }
+  //other methods
+}
+
+let stackOne = new Stack();
+let stackTwo = new Stack();
+
+stackOne.push(23);
+stackTwo.push(22);
+stackTwo.push(2);
+console.log(items);
+console.log(stackOne.pop());
+console.log(stackTwo.pop());
